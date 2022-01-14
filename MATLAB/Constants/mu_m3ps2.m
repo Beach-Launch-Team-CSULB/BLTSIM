ba9d9ad8c9295ffd23a242b6bb17db6fbf0eef65
Mat Arnold <mat.arnold.93@gmail.com>
2022-01-13 18:16:24 -0800

    added a function to calculate ellipsoidal gravity accleration

---
+++ MATLAB/Constants/mu_m3ps2.m
@@ -0,0 +1,5 @@
+function mu = mu_m3ps2()
+% Earth Gravitational Constant
+% From: https://www.vcalc.com/wiki/vCalc/Earth+Gravitational+Constant+%28%60mu%60%29+%3A+%60m%5E3%60%2F%60sec%5E2%60
+    mu = 3.98574405096e14;
+end
